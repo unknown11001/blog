681948435dfd8422bcf18c08e4542d30ee212262
Bunnys666 <muhammadryanapriyudan@gmail.com>
2021-05-01 10:36:41 +0700

    Release

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210501.1032';
+const cacheName = 'chirpy-20210501.1036';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
