5cc82f051906e10cd86dc5368577191495d85322
Bunnys666 <muhammadryanapriyudan@gmail.com>
2021-05-01 09:51:33 +0700

    Release

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -58,7 +58,7 @@ const allowedDomains = [
     'www.google-analytics.com',
   
 
-  'unknown11001.github.io/',
+  'unknown11001.github.io',
 
   'fonts.gstatic.com',
   'fonts.googleapis.com',
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210430.2150';
+const cacheName = 'chirpy-20210501.0951';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
